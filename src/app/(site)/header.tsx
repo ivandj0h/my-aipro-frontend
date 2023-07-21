cc3340afd0060cce092ba27023daacd90d8aaca1
ivandjoh <ivandi.djoh@gmail.com>
2023-07-21 09:46:38 +0800

    updated header

--- src/app/(site)/header.tsx
+++ src/app/(site)/header.tsx
@@ -1,27 +1,28 @@
-import React, { JSX } from "react";
+import React, {JSX} from "react";
 import Link from "next/link";
-import LoginButton from "../components/auth/SignInButton";
+import LoginButton from "@/app/components/auth/SignInButton";
 
 
 const Header: React.FC = (): JSX.Element => {
     return (
-        <header className="bg-stone-100 py-10">
-            <nav className="container flex items-center text-sm font-medium tracking-wider uppercase text-stone-500">
-                <ul className="ml-auto flex justify-center gap-8">
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/accounts">Account</Link>
-                    </li>
-                </ul>
-                <ul className="ml-auto">
-                    <li>
-                        <LoginButton />
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <main className="fixed w-full z-50">
+            <header className="py-4 bg-white sticky top-0 z-50 drop-shadow-lg">
+                <nav
+                    className="container flex items-center text-sm font-medium tracking-wider uppercase text-stone-500">
+                    <ul className="ml-auto flex justify-end items-center  gap-8">
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/accounts">Account</Link>
+                        </li>
+                        <li>
+                            <LoginButton/>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </main>
     )
 }
 
--- src/app/components/auth/SignInButton.tsx
+++ src/app/components/auth/SignInButton.tsx
@@ -10,18 +10,18 @@ const SignInButton: React.FC = (): JSX.Element => {
 
     if (session) {
         return (
-            <div className="relative h-12 w-12">
+            <div className="relative h-8 w-8">
                 {session.user?.image ? (
                     <Image
                         src={session.user.image}
                         alt={session.user.name || session.user.email || 'User'}
-                        className="inline-block h-12 w-12 rounded-full"
+                        className="inline-block h-8 w-8 rounded-full"
                         layout='fill'
                         objectFit='cover'
                     />
 
                 ) : (
-                    <span className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
+                    <span className="inline-block h-8 w-8 overflow-hidden rounded-full bg-gray-100">
                     <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
                         <path
                             d='M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z'/>
@@ -34,7 +34,7 @@ const SignInButton: React.FC = (): JSX.Element => {
 
     return (
         <button
-            className="text-sm font-medium tracking-wider uppercase text-stone-500"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             onClick={() => signIn()}
         >
             Sign in
