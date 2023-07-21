9a304b94d089b88f05ac43c6646a886fea54cf49
ivandjoh <ivandi.djoh@gmail.com>
2023-07-21 09:23:32 +0800

    updated login

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TextField from "@/app/components/form/TextField";
 import Button from "@/app/components/ui/button";
+import GoogleLoginButton from "@/app/components/auth/GoogleSignInButton";
 
 
 const SignInPage: React.FC = (): JSX.Element => {
@@ -27,6 +28,10 @@ const SignInPage: React.FC = (): JSX.Element => {
                     Continue with email
                 </Button>
             </form>
+            <div className='mx-auto my-10 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400'>
+                or
+            </div>
+            <GoogleLoginButton />
         </>
     )
 }
